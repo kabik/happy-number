ea47a10d45c0d46d6d1139969adf2e5326451fd4
kabik <k2.s1k.x20@gmail.com>
2020-03-17 01:01:49 +0900

    output image

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-N = 1000000
+from graphviz import Digraph
+
+N = 1000
+edges = []
 memo = {1: True}
 
+def sum_digit_square(n):
+    sum = 0
+    for i in str(n):
+        k = int(i)
+        sum += k ** 2
+    return sum
+
+
 def is_happy(n):
-    #print(memo)
     appeared = []
 
     while True:
@@ -17,17 +27,27 @@ def is_happy(n):
                 memo[n] = False
             return False
 
+        next = sum_digit_square(n)
         appeared.append(n)
 
-        sum = 0
-        for i in str(n):
-            k = int(i)
-            sum += k ** 2
-
-        n = sum
+        edge = (n, next)
+        if edge not in edges:
+            edges.append((n, next))
+        n = next
 
 
-for i in range(1, N):
-    if is_happy(i):
-        pass
-        #print(i)
+def main():
+    for i in range(1, N):
+        if is_happy(i):
+            #print(i)
+            pass
+
+    G = Digraph(format="png")
+    G.attr("node", shape="circle")
+    for i,j in edges:
+        if memo[i]:
+            G.edge(str(i), str(j))
+    G.render("output/tree_" + str(N))
+
+if __name__ == "__main__":
+    main()
